b07f11231c4d4a6648b5e485843624e4ac061def
David Guzmán <djguzmanc@unal.edu.co>
2017-11-12 17:24:41 -0500

    Big commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var app = express()
 const PORT = 3003;
 
 // connect to DB
-mongoose.connect('mongodb://information_db:3305/songs_information');
+mongoose.connect('mongodb://information-db:3305/songs_information');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
